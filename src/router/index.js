694fae94d204a5574060bff75619767279ddba00
Xue <545589205@qq.com>
2018-01-31 23:26:43 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MallPage from './../views/MallPage.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'MallPage',
+      component: MallPage
     }
   ]
 })
---
+++ src/views/MallPage.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div id="container">
+    <!-- <div class="item" v-for="item in itemList">
+      <div class="itemName">{{item.name}}</div>
+      <div class="itemPrice">{{item.price}}</div>
+      <br>
+      <div class="smallbox"></div>
+
+
+
+
+    </div> -->
+    hah
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name:'MallPage',
+  el:'container',
+  data:function aa(){
+    return  {
+      itemList: [],
+      msg: "jasdia"
+    }
+  },
+   mounted(){
+      this.itemList = [{
+        "name": "heihei",
+        "price": 2000
+      },{
+        "name": "s",
+        "price": 2
+      },{
+        "name": "s",
+        "price": 2
+      },{
+        "name": "s",
+        "price": 2
+      }]
+    }
+
+}
+</script>
+<style>
+*{
+      margin:0px;
+      padding:0px;
+    }
+    #container{
+      display: flex;
+      width:100%;
+      flex-wrap:wrap;
+    }
+    .item{
+      margin:10px;
+      width:200px;
+      min-width: 200px;
+      border:5px solid gray;
+      height:200px;
+      transition:all .6s;
+      text-align: center;
+      }
+    .item:hover{
+      border:5px solid #00BFFF;
+      /* transition:all 5s; */
+      /* transform: scale(1.01) */
+      box-shadow: 10px 10px 5px #888888;
+      /* transform:rotate(7deg) */
+      transform:translateY(-10px)
+    }
+    .item:hover .smallbox{
+
+      /* transition:all 5s; */
+      /* transform: scale(1.01) */
+      /* box-shadow: 10px 10px 5px #888888; */
+      /* transform:rotate(7deg) */
+      /* transform:translateY(-10px); */
+      transform:scale(1.3)
+
+    }
+    .smallbox{
+      display:inline-block;
+      height:50px;
+      width:50px;
+      border:2px solid grey;
+      text-align: center;
+      transition:all .6s;
+      background: red;
+
+    }
+</style>
