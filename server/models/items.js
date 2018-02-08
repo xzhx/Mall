10769ba6cd0dcad447cd13609d56c647a95153c7
Xue <545589205@qq.com>
2018-02-08 17:24:56 +0800

    定义商品的模型schema

---
+++ server/models/items.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+
+var Schema = mongoose.Schema;//Schema定义表的模型
+
+var itemsSchema = new Schema({
+  "productId" : String,
+  "productName" : String,
+  "salePrice" : Number,
+  "productImage" : String
+})
+
+module.exports = mongoose.model('item',itemsSchema)
